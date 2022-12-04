055827b0fe3d8d1269afd529d44ea809b1755794
j-stroup <jeffreyrstroup@gmail.com>
2022-12-04 14:20:56 -0500

    Added 404 handling

--- main.py
+++ main.py
@@ -39,83 +39,105 @@ def check_sites_multiple():
     for item in urls:
         url = item.strip()
         r = requests.get(url)
-        page_source = r.text.encode('utf-8')
-        hash = hashlib.sha256(page_source).hexdigest()
-        print(f'\n{item}')
-        print(hash)
-        list.update({url:hash})
+        response = str(r)
+        response = response.strip('<Response []>')
+        if response != '200':
+            print(url.replace('https://','') + ' - Error')
+        else:
+            page_source = r.text.encode('utf-8')
+            hash = hashlib.sha256(page_source).hexdigest()
+            print(f'\n{item}')
+            print(hash)
+            list.update({url:hash})
     count = 0
     for key in list:
         count = count + 1
     while True:
         time.sleep(frequency)
         for url in urls:
+            obj = time.localtime()
+            t = time.asctime(obj)
             hash = list.get(url)
             r = requests.get(url)
-            page_source = r.text.encode('utf-8')
-            hash_check = hashlib.sha256(page_source).hexdigest()
-            if hash_check != hash:
-                obj = time.localtime()
-                t = time.asctime(obj)
-                print(t + ' - ' + url.replace('https://','') + ' has been updated')
-                hash = hash_check
+            response = str(r)
+            response = response.strip('<Response []>')
+            if response != '200':
+                print(t + ' - ' + url.replace('https://','') + ' - Error')
             else:
-                obj = time.localtime()
-                t = time.asctime(obj)
-                print(t + ' - No update')
+                page_source = r.text.encode('utf-8')
+                hash_check = hashlib.sha256(page_source).hexdigest()
+                if hash_check != hash:
+                    print(t + ' - ' + url.replace('https://','') + ' has been updated')
+                    hash = hash_check
+                else:
+                    print(t + ' - No update')
 
 def check_single_site(url, frequency):
     r = requests.get(url)
-    page_source = r.text.encode('utf-8')
-    hash = hashlib.sha256(page_source).hexdigest()
-    print(f'\n{url}')
-    print(hash)
-    print('\n')
-    while True:
-        time.sleep(frequency)
-        r = requests.get(url)
+    response = str(r)
+    response = response.strip('<Response []>')
+    if response != '200':
+        print(r + ' - ' + url.replace('https://','') + ' - Error')
+    else:
         page_source = r.text.encode('utf-8')
-        hash_check = hashlib.sha256(page_source).hexdigest()
-        if hash_check != hash:
-            obj = time.localtime()
-            t = time.asctime(obj)
-            print(t + ' - Page has been updated')
-            hash = hash_check
-        else:
-            obj = time.localtime()
-            t = time.asctime(obj)
-            print(t + ' - No update')
+        hash = hashlib.sha256(page_source).hexdigest()
+        print(f'\n{url}')
+        print(hash)
+        print('\n')
+        while True:
+            time.sleep(frequency)
+            r = requests.get(url)
+            response = str(r)
+            response = response.strip('<Response []>')
+            if response != '200':
+                print(t + ' - ' + url.replace('https://','') + ' - Error')
+            else:
+                page_source = r.text.encode('utf-8')
+                hash_check = hashlib.sha256(page_source).hexdigest()
+                if hash_check != hash:
+                    print(t + ' - Page has been updated')
+                    hash = hash_check
+                else:
+                    print(t + ' - No update')
 
 def check_sites_file(frequency):
     list = {}
     for item in urls:
         url = item.strip()
         r = requests.get(url)
-        page_source = r.text.encode('utf-8')
-        hash = hashlib.sha256(page_source).hexdigest()
-        print(f'\n{item}')
-        print(hash)
-        list.update({url:hash})
+        response = str(r)
+        response = response.strip('<Response []>')
+        if response != '200':
+            print(url.replace('https://','') + ' - Error')
+        else:
+            page_source = r.text.encode('utf-8')
+            hash = hashlib.sha256(page_source).hexdigest()
+            print(f'\n{item}')
+            print(hash)
+            list.update({url:hash})
     print('\n')
     count = 0
     for key in list:
         count = count + 1
     while True:
+        obj = time.localtime()
+        t = time.asctime(obj)
         time.sleep(frequency)
         for url in urls:
             hash = list.get(url)
             r = requests.get(url)
-            page_source = r.text.encode('utf-8')
-            hash_check = hashlib.sha256(page_source).hexdigest()
-            if hash_check != hash:
-                obj = time.localtime()
-                t = time.asctime(obj)
-                print(t + ' - ' + url.replace('https://','') + ' has been updated')
-                hash = hash_check
+            response = str(r)
+            response = response.strip('<Response []>')
+            if response != '200':
+                print(t + ' - ' + url.replace('https://','') + ' - Error')
             else:
-                obj = time.localtime()
-                t = time.asctime(obj)
-                print(t + ' - No update')
+                page_source = r.text.encode('utf-8')
+                hash_check = hashlib.sha256(page_source).hexdigest()
+                if hash_check != hash:
+                    print(t + ' - ' + url.replace('https://','') + ' has been updated')
+                    hash = hash_check
+                else:
+                    print(t + ' - No update')
 
 
 start()
